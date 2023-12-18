b4a768e824aa48e39d4939117ccebb83953a325d
felipe <felipenegrelleramos@gmail.com>
2023-12-18 10:04:35 -0300

    init

---
+++ src/test/java/com/felipe/springboot/server/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.springboot.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
